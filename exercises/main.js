f46765433be25a8c9112fe76a27f3b5a544c3488
Jacob DeFalco <jacob.defalco97@gmail.com>
2023-04-25 11:06:27 -0400

    added money stuff

--- exercises/main.js
+++ exercises/main.js
@@ -6,4 +6,12 @@ console.log(num1 ** 2);
 var firstName, lastName;
 firstName = "Jake";
 lastName = "DeFalco";
-console.log("Hello, traveler! You must be" + " " + firstName + " " + lastName + "!" );
+console.log("Hello, traveler! You must be" + " " + firstName + " " + lastName + "!");
+
+var foodCost = 89.99;
+var tax = foodCost * 0.07;
+var tip = tax * 0.15;
+
+var totalDue = foodCost + tax + tip;
+
+console.log("The total due is " + "$" + totalDue.toFixed(2));
