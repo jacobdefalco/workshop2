b21976276bea51de6dbc953223c56600d56d5ba2
Jacob DeFalco <jacob.defalco97@gmail.com>
2023-04-25 13:17:46 -0400

    fixed denominator

--- exercises/rule_of_72.js
+++ exercises/rule_of_72.js
@@ -1,6 +1,6 @@
 "use strict";
 var numOfPeriods, investment, rateOfReturn;
 investment = 250000;
-rateOfReturn = 0.03;
+rateOfReturn = 3;
 numOfPeriods = 72 / rateOfReturn;
-console.log("It is going to take " + numOfPeriods + " months to double your initial investment of " + investment + ".");
+console.log("It is going to take " + numOfPeriods + " years to double your initial investment of " + investment + ".");
