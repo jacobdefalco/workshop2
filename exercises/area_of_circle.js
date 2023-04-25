da3019b6be23cbfabe145f41379eb1e613aef3d6
Jacob DeFalco <jacob.defalco97@gmail.com>
2023-04-25 13:17:39 -0400

    added math.PI

--- exercises/area_of_circle.js
+++ exercises/area_of_circle.js
@@ -1,5 +1,5 @@
 "use strict";
 var areaOfCircle, radius;
 radius = 16;
-areaOfCircle = 3.14159 * radius ** 2;
+areaOfCircle = Math.PI * radius ** 2;
 console.log("The area of the circle is " + areaOfCircle.toFixed(2) + "in");
