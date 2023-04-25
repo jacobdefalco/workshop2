31ed83ea041cf91dfed85dcb86a065b3e1b34427
Jacob DeFalco <jacob.defalco97@gmail.com>
2023-04-25 10:31:56 -0400

    added script

---
+++ exercises/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var num1;
+num1 = 13;
+console.log(num1 ** 2);
+
+var firstName, lastName;
+firstName = "Jake";
+lastName = "DeFalco";
+console.log("Hello, traveler! You must be" + " " + firstName + " " + lastName + "!" );
--- scripts/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var num1;
-num1 = 13;
-console.log(num1 ** 2);
