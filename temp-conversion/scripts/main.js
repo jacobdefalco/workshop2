2dc86ad0bb9f391ea0c47b712205709921513277
Jacob DeFalco <jake.defalco97@gmail.com>
2023-04-28 15:01:11 -0400

    finish miniproj

--- temp-conversion/scripts/main.js
+++ temp-conversion/scripts/main.js
@@ -5,12 +5,12 @@ const result = document.getElementById("answer");
 
 function convertFtoC() {
   let tempF = tempInF.value;
-  let tempC = (tempF - 32) * (5 / 9);
-  result.innerText = tempC;
+  let tempC = parseInt((tempF - 32) * (5 / 9));
+  result.innerText = `${tempF}°F converted into Fahrenheit would be ${tempC}°C!`;
 }
 
 function convertCtoF() {
   let tempC = tempInC.value;
-  let tempF = tempC * (9 / 5) + 32;
-  result.innerText = tempF;
+  let tempF = parseInt(tempC * (9 / 5) + 32);
+  result.innerText = `${tempC}°C converted into Fahrenheit would be ${tempF}°F!`;
 }
